9b79bb1868824b8d46ef192e391f804c512e6c7c
Dawid Bitner <dawbitner@gmail.com>
2022-10-01 13:12:37 +0200

    Lekcja 3
    - dodano pauzę
    - dodano timer
    - dodano podłogi
    - dodano assety
    - dodano tagi
    - dodano warstwy
    - dodano podstawową logikę działania gry 3D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     Vector3 velocity; // posłuży do wyliczenia prędkości w dół
     CharacterController characterController; // komponent CHaracter Controller
 
+    public Transform groundCheck; // miejsce na nasz obiekt
+    public LayerMask groundMask; // grupa obiektów, które będą warstwą używane za teren
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +29,25 @@ public class PlayerController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
         characterController.Move(move * speed * Time.deltaTime);
+
+        RaycastHit hit; // zmienna w które zapisywana jest referencja do uderzonego obiektu
+        if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groundMask)) 
+        {
+            string terrainType;
+            terrainType = hit.collider.gameObject.tag; //sprawdzamy tag tego w co uderzyliśmy
+
+            switch (terrainType) 
+            {
+                default:        // standardowa prędkość gdy chodzimy po dowolnym terenie
+                    speed = 12;
+                    break;
+                case "Low":    //prędkość gdy chodzimy po terenie spowalniającym
+                    speed = 3;
+                    break;
+                case "High":   // prędkość gdy chodzimy po terenie przyspieszającym
+                    speed = 20;
+                    break;
+            }
+        }
     }
 }
