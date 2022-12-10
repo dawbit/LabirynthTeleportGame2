817487257200655cd43a9de2f27a203827d069f5
Dawid Bitner <dawbitner@gmail.com>
2022-12-10 13:21:11 +0100

    Lekcja 12
    - maze generator
    - skybox

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,88 +1,109 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class LevelGenerator : MonoBehaviour
 {
     public Texture2D map;
-    public ColorToPrefarb[] collorMappings;
+    public ColorToPrefarb[] colorMappings;
     public float offset = 5f;
 
     public Material material01;
     public Material material02;
 
-    void GenerateTile(int x, int z)
-    {
-        //Pobieramy kolor pixela w pozycji x i z
-        Color pixelColor = map.GetPixel(x, z);
-
-        //Jeżeli alpha koloru jest równa 0, czyli kolor jest w pełni przeźroczysty to pomijamy pixel
-        if(pixelColor.a == 0) 
-        {
-            return;
-        } 
-
-        //Przeszukiwanie po wszystkich kolorach w naszej tablicy
-        foreach(ColorToPrefarb collorMapping in collorMappings)
-        {
-            //Jeżeli któryś z kolorów w naszej tablicy odpowiada to ustaw prefarb
-            if(collorMapping.color.Equals(pixelColor))
-            {
-                //wylicz pozycje na podstawie współrzędnej pixela
-                Vector3 position = new Vector3(x, 0, z) * offset;
-                // Utwórz wybrany obiekt w wybranej pozycji
-                Instantiate(collorMapping.prefarb, position, Quaternion.identity, transform);
-            }
-        }
-    }
+ 
 
     public void GenerateLabirynth()
     {
+        Debug.Log("Liczba pixeli: szerokość " + map.width + " i wysokość " + map.height + ". Łącznie: " + (map.width * map.height) + " pixeli");
+
         for(int x = 0; x < map.width; x++)
         {
-            for(int z = 0; z < map.height; z++)
+            for (int z = 0; z < map.height; z++)
             {
+                //Debug.Log("Generuje blok o wspórzędnych: x " + x + " i z " + z);
+                //Debug.Log("Numer bloku: " + (x * map.width + z + 1) +" z : " + (map.width * map.height));
                 GenerateTile(x,z);
             }
         }
 
+
         ColorTheChildren();
+
+
+    }
+
+    void GenerateTile(int x, int z)
+    {
+        Color pixelColor = map.GetPixel(x, z);
+
+        /*if(pixelColor.a == 0)
+        {
+            return;
+        }*/
+        bool pyklo = false;
+        foreach(ColorToPrefarb colorMapping in colorMappings)
+        {
+            if(colorMapping.color.Equals(pixelColor))
+            {
+                Vector3 position = new Vector3(x, 0, z) * offset;
+                Instantiate(colorMapping.prefarb, position, Quaternion.identity, transform);
+                pyklo = true;
+                //Debug.Log("Pykło: x " + x + " z " + z + ". Kolor pixela: R " + pixelColor.r + " G " + pixelColor.g + " B " + pixelColor.b + " A " + pixelColor.a);
+                Debug.Log("Pykło: x " + x + " z " + z + ". Kolor pixela: " +  pixelColor.ToString());
+
+            }
+        }
+
+        if(!pyklo)
+        {
+            //Debug.LogWarning("Coś nie pykło: x " + x + " z " + z + ". Kolor pixela: R " + pixelColor.r + " G " + pixelColor.g + " B " + pixelColor.b + " A " + pixelColor.a);
+            Debug.LogWarning("Coś nie pykło: x " + x + " z " + z + ". Kolor pixela: " + pixelColor.ToString());
+        }
     }
 
     public void ColorTheChildren()
     {
-        foreach(Transform child in transform) 
+        foreach (Transform child in transform)
         {
-            if (child.tag == "Wall") 
+            if (child.tag == "Wall")
             {
-                if (Random.Range(1, 100) % 3 == 0) 
+                if(Random.Range(1,100) % 3 == 0)
                 {
                     child.gameObject.GetComponent<Renderer>().material = material02;
                 }
-                else {
+                else
+                {
                     child.gameObject.GetComponent<Renderer>().material = material01;
                 }
+                
             }
 
-            //W tym miejscu można dodać kawałek kodu, który będzie sprawdzał czy ściana ma jeszcze jakieś dzieci i by im
-            //też nadała materiały w ten sposób
+            //W tym miejscu można dodać kawałek kodu, 
+            //który będzie sprawdzał czy ściana ma jeszcze jakieś dzieci i by im też nadała materiały w ten sposób
+            //opis poniżej
 
-            if(child.childCount > 0)
+            if (child.childCount > 0)
             {
-                foreach(Transform grandchild in child.transform)
+                foreach (Transform grandchild in child.transform)
                 {
-                    if (grandchild.tag == "Wall") 
+                    if (grandchild.tag == "Wall")
                     {
-                        if(Random.Range(1, 100) % 3 == 0) 
+                        if (Random.Range(1, 100) % 3 == 0)
                         {
                             grandchild.gameObject.GetComponent<Renderer>().material = material02;
                         }
-                        else {
+                        else
+                        {
                             grandchild.gameObject.GetComponent<Renderer>().material = material01;
                         }
                     }
                 }
             }
+
         }
     }
+
+
 }
